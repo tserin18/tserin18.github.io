0ad7763a94672efa5160b37736c158f6db843f39
tserin18 <sherpa.tsering2011@gmail.com>
2018-04-22 11:46:52 -0700

    email

--- php/contact.php
+++ php/contact.php
@@ -1,79 +1,85 @@
 <?php
-if(isset($_POST['email'])) {
- 
-    // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "sherpa.tsering2011@gmail.com";
-    $email_subject = "New portfolio email from " + $_POST['name'];
- 
-    function died($error) {
-        // your error code can go here
-        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
-        echo "These errors appear below.<br /><br />";
-        echo $error."<br /><br />";
-        echo "Please go back and fix these errors.<br /><br />";
-        die();
-    }
- 
- 
-    // validation expected data exists
-    if(!isset($_POST['name']) ||
-        !isset($_POST['email']) ||
-        !isset($_POST['comments'])) {
-        died('We are sorry, but there appears to be a problem with the form you submitted.');       
-    }
- 
-     
- 
-    $name = $_POST['name']; // required
-    $email = $_POST['email']; // required
-    $comments = $_POST['comments']; // required
- 
-    $error_message = "";
-    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
- 
-  if(!preg_match($email_exp,$email)) {
-    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
-  }
- 
-    $string_exp = "/^[A-Za-z .'-]+$/";
- 
-  if(!preg_match($string_exp,$name)) {
-    $error_message .= 'The First Name you entered does not appear to be valid.<br />';
-  }
- 
-  if(strlen($comments) < 2) {
-    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
-  }
- 
-  if(strlen($error_message) > 0) {
-    died($error_message);
-  }
- 
-    $email_message = "Form details below.\n\n";
- 
-     
-    function clean_string($string) {
-      $bad = array("content-type","bcc:","to:","cc:","href");
-      return str_replace($bad,"",$string);
-    }
- 
-     
- 
-    $email_message .= "Name: ".clean_string($name)."\n";
-    $email_message .= "Email: ".clean_string($email)."\n";
-    $email_message .= "Comments: ".clean_string($comments)."\n";
- 
-// create email headers
-$headers = 'From: '.$email_from."\r\n".
-'Reply-To: '.$email."\r\n" .
-'X-Mailer: PHP/' . phpversion();
-@mail($email_to, $email_subject, $email_message, $headers);  
-?>
 
-Hi there,
-Your message has been sent successfully. Thank you for contacting me. I will be in touch with you very soon.
- 
-<?php
+if(!$_POST) exit;
+
+// Email address verification, do not edit.
+function isEmail($email) {
+	return(preg_match("/^[-_.[:alnum:]]+@((([[:alnum:]]|[[:alnum:]][[:alnum:]-]*[[:alnum:]])\.)+(ad|ae|aero|af|ag|ai|al|am|an|ao|aq|ar|arpa|as|at|au|aw|az|ba|bb|bd|be|bf|bg|bh|bi|biz|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|com|coop|cr|cs|cu|cv|cx|cy|cz|de|dj|dk|dm|do|dz|ec|edu|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gh|gi|gl|gm|gn|gov|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|in|info|int|io|iq|ir|is|it|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|mg|mh|mil|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|museum|mv|mw|mx|my|mz|na|name|nc|ne|net|nf|ng|ni|nl|no|np|nr|nt|nu|nz|om|org|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|pro|ps|pt|pw|py|qa|re|ro|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|sk|sl|sm|sn|so|sr|st|su|sv|sy|sz|tc|td|tf|tg|th|tj|tk|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|um|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)$|(([0-9][0-9]?|[0-1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5])\.){3}([0-9][0-9]?|[0-1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5]))$/i",$email));
+}
+
+if (!defined("PHP_EOL")) define("PHP_EOL", "\r\n");
+
+$name     = $_POST['name'];
+$email    = $_POST['email'];
+$comments = $_POST['comments'];
+
+if(trim($name) == '') {
+	echo '<div class="error_message">You must enter your name.</div>';
+	exit();
+} else if(trim($email) == '') {
+	echo '<div class="error_message">Please enter a valid email address.</div>';
+	exit();
+} else if(!isEmail($email)) {
+	echo '<div class="error_message">You have entered an invalid e-mail address. Please try again.</div>';
+	exit();
+}
+
+if(trim($comments) == '') {
+	echo '<div class="error_message">Please enter your message.</div>';
+	exit();
+}
+
+if(get_magic_quotes_gpc()) {
+	$comments = stripslashes($comments);
+}
+
+
+// Configuration option.
+// Enter the email address that you want to emails to be sent to.
+// Example $address = "joe.doe@yourdomain.com";
+
+//$address = "example@example.net";
+$address = "sherpa.tsering2011@gmail.com";
+
+
+// Configuration option.
+// i.e. The standard subject will appear as, "You've been contacted by John Doe."
+
+// Example, $e_subject = '$name . ' has contacted you via Your Website.';
+
+$e_subject = 'You have been contacted by ' . $name . '.';
+$e_subject = $name . ' has contacted you via Your Website.';
+
+// Configuration option.
+// You can change this if you feel that you need to.
+// Developers, you may wish to add more fields to the form, in which case you must be sure to add them here.
+
+$e_body = "You have been contacted by $name. Their additional message is as follows." . PHP_EOL . PHP_EOL;
+$e_content = "\"$comments\"" . PHP_EOL . PHP_EOL;
+$e_reply = "You can contact $name via email, $email";
+
+$msg = wordwrap( $e_body . $e_content . $e_reply, 70 );
+
+$headers = "From: $email" . PHP_EOL;
+$headers .= "Reply-To: $email" . PHP_EOL;
+$headers .= "MIME-Version: 1.0" . PHP_EOL;
+$headers .= "Content-type: text/plain; charset=utf-8" . PHP_EOL;
+$headers .= "Content-Transfer-Encoding: quoted-printable" . PHP_EOL;
+
+if(mail($address, $e_subject, $msg, $headers)) {
+
+	// Email has sent successfully, echo a success page.
+
+	echo "<fieldset>";
+	echo "<div id='success_page'>";
+	echo "<h3>Email Sent Successfully.</h3>";
+	echo "<p>Thank you <strong>$name</strong>, your message has been submitted to us.</p>";
+	echo "</div>";
+	echo "</fieldset>";
+
+} else {
+
+	echo 'ERROR!';
 
 }
 ?>
